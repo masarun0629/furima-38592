bcfa6b2081bbbeb46f4c000fcafec68d8414d3e4
masarun0629 <o8058642424@gmail.com>
2022-10-21 19:25:35 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/} do
+  validates :first_name
+  validates :last_name
+end  
+with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+  validates :first_name_kana
+  validates :last_name_kana
+end
+  validates :birth_day, presence: true
+end
