1f2786fea64a8257ac38876224145bc291549da8
masarun0629 <o8058642424@gmail.com>
2022-11-02 17:09:54 +0900

    ページ遷移の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,7 @@ class ItemsController < ApplicationController
  end 
  
  def edit
-  unless current_user.id == @item.user.id
+  unless current_user.id == @item.user.id && @item.order == nil
     redirect_to root_path 
   end
  end
